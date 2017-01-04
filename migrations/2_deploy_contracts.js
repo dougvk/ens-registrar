7382f2df773e168b7871d6459ae5b1d2d8c765b9
Doug von Kohorn <dougvk@gmail.com>
2017-01-04 08:31:00 -0500

    Add starting auction to library.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-// The hex string reolves to the TLD 'eth'
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.autolink();
   deployer.deploy(MetaCoin);
+
+  // The hex string reolves to the TLD 'eth'
   deployer.deploy(ENS, '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae')
     .then(function() {
-      //deployer.deploy(TestRegistrar, ENS.deployed().address, '0x04f740db81dc36c853ab4205bddd785f46e79ccedca351fc6dfcbd8cc9a33dd6');
       deployer.deploy(Registrar, ENS.deployed().address, '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae');
     });
   deployer.deploy(PublicResolver);
--- test/ENS.js
+++ test/ENS.js
@@ -19,12 +19,23 @@ contract("ENS integration", function(accounts) {
     auctionRegistrar = new ENSAuction(web3, Registrar.deployed().address);
   });
 
-  it("registers the TLD test", function() {
+  it("demonstrates that the domain name isn't up for auction", function() {
     //TestRegistrar.deployed().register(web3.sha3('lame'), accounts[0])
       //.then(function() {
         //assert.equal(ens.owner('lame.test'), accounts[0], "The owner of lame.test isn't correct");
       //});
     // 0 means the name is avaiable and no auction has been started.
-    assert.equal(auctionRegistrar.registrar.entries(web3.sha3('lame'))[0], 0);
+    assert.isTrue(auctionRegistrar.available('test'), 'test is available as a domain name');
+    assert.isFalse(auctionRegistrar.upForAuction('test'), 'test isn\'t up for auction');
+  });
+
+  it("can start an auction", function() {
+    //var auctionStartedEvent = Registrar.deployed().AuctionStarted(function(err, res) {
+      //assert.isNull(err, 'no error on the auction');
+      //assert.equal(res.args.hash, web3.sha3('test'), 'res created the auction for the name `test`');
+    //});
+    auctionRegistrar.startAuction('test', {from: accounts[0], gas: 1000000}, function() {
+      assert.isTrue(auctionRegistrar.upForAuction('test'), '`test` is officially up for auction');
+    });
   });
 });
