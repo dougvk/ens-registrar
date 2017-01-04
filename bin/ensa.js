cf5cebf434d878f5a3465dc3a665663e35c8b691
Doug von Kohorn <dougvk@gmail.com>
2017-01-04 10:15:33 -0500

    Update first command.

---
+++ bin/ensa.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+var yargs = require('yargs');
+var auctionRegistar = require('../lib/ensAuction');
+
+var args = yargs
+  .command('bid', 'Place a bid on a domain name', function(yargs) {
+    return yargs.option('host', {
+      description: "HTTP host of Ethereum node",
+      alias: 'h',
+      default: 'testrpc'
+    })
+    .option('port', {
+      description: "HTTP port",
+      alias: 'p',
+      default: '8545'
+    })
+    .option('name', {
+      description: "The name you want to register",
+      alias: 'n',
+      type: "string"
+    })
+    .option('account', {
+      description: "The address to register the domain name",
+      alias: 'a',
+      type: "string"
+    })
+    .option('max', {
+      description: "The maximum amount willing to pay for the name",
+      alias: 'm',
+      type: 'number'
+    })
+    .option('salt', {
+      description: "The secret 'salt' for unsealing your bid",
+      alias: 's',
+      type: 'string'
+    })
+    .demand(['account', 'max', 'salt', 'name'])
+  })
+  .help()
+  .usage("Usage: $0 [command] [options]");
+
+var argv = args.argv;
+
+if (argv._.length == 0) {
+  args.showHelp();
+}
+
+var command = argv._[0];
